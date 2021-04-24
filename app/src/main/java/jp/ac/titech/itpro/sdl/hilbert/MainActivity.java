8afddb50582b29c1a465856ce1d9a85fb6cd0bb2
廣畑和樹 <ttianzhong539@gmail.com>
2021-04-24 16:42:39 +0900

    回転しても次数を保持するように変更

--- app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private final static int MAX_ORDER = 9;
+    private final static String KEY_ORDER = "MainActivity.order";
     private int order = 1;
 
     private TextView orderView;
@@ -21,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            order = savedInstanceState.getInt(KEY_ORDER);
+        }
 
         orderView = findViewById(R.id.order_view);
         hilbertView = findViewById(R.id.hilbert_view);
@@ -49,6 +54,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         display();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_ORDER, order);
     }
 
     private void display() {
